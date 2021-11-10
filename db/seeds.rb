f55d76239a376c61a1a0b3eac234a340f2daf6d6
umberto najar <najar.umberto@gmail.com>
2021-11-10 15:54:04 -0500

    created the reviews controller & both new and create methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create!(name: 'Le Filet', address: 'mont-royal', phone_number: '5149450113', category: 'japanese')
+Restaurant.create!(name: 'Bouillon Bilk', address: 'saint-laurent', phone_number: '5149450301', category: 'chinese')
+Restaurant.create!(name: 'India Rosa', address: 'milton', phone_number: '5149450111', category: 'italian')
+Restaurant.create!(name: 'Panthere Verte', address: 'casgrain', phone_number: '5149450112', category: 'french')
+Restaurant.create!(name: 'Luna', address: 'rachel', phone_number: '5149450115', category: 'belgian')
